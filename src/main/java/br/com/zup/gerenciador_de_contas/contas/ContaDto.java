a8daacf6d2cd4a7dca94d3547dec27a2a58b5f87
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-10 20:52:10 -0300

    Criar classe ContaDto

---
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaDto.java
@@ -0,0 +1,46 @@
+package br.com.zup.gerenciador_de_contas.contas;
+
+import java.time.LocalDate;
+
+public class ContaDto {
+
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+
+    public ContaDto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+}
