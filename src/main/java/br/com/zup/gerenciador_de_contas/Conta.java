d9a44cb4236bfc3e71b7e3640e950233e80746ba
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-10 20:32:12 -0300

    Criar Classe Conta

---
+++ src/main/java/br/com/zup/gerenciador_de_contas/Conta.java
@@ -0,0 +1,82 @@
+package br.com.zup.gerenciador_de_contas;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contas")
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private double valor;
+    @Column(nullable = false)
+    private Tipo tipo;
+    private Status status;
+    @Column(nullable = false)
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+
+    public Conta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+}
