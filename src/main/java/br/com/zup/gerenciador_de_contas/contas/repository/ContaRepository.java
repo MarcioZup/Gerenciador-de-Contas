1e71fe5ec1e26b6bc3c17d9547b1ff695627b304
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-12 19:50:06 -0300

    Criar Lista para achar contas por Status

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/repository/ContaRepository.java
@@ -1,8 +1,12 @@
 package br.com.zup.gerenciador_de_contas.contas.repository;
 
 import br.com.zup.gerenciador_de_contas.contas.models.Conta;
+import br.com.zup.gerenciador_de_contas.contas.models.Status;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
+    List<Conta> findAllByStatus(Status status);
 }
