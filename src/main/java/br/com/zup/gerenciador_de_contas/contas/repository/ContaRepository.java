cff049cab4834352027cb1601f0fd157356de18c
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-12 19:50:52 -0300

    Criar Lista para achar contas por Tipo

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/repository/ContaRepository.java
@@ -2,6 +2,7 @@ package br.com.zup.gerenciador_de_contas.contas.repository;
 
 import br.com.zup.gerenciador_de_contas.contas.models.Conta;
 import br.com.zup.gerenciador_de_contas.contas.models.Status;
+import br.com.zup.gerenciador_de_contas.contas.models.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
     List<Conta> findAllByStatus(Status status);
+
+    List<Conta> findAllByTipo(Tipo tipo);
 }
